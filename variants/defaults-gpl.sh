13eac3bd7dc50579316f42111e20f81eaeb08481
TheSecondAkari <49018872+TheSecondAkari@users.noreply.github.com>
2026-09-13 03:27:34 +0800

    Update defaults-gpl.sh

--- variants/defaults-gpl.sh
+++ variants/defaults-gpl.sh
@@ -1,6 +1,10 @@
-FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug"
+#!/bin/bash
+# 引用集中配置文件（修改 FFmpeg 编译参数请编辑根目录 ffmpeg-custom-config.sh）
+source "$(dirname "${BASH_SOURCE[0]}")/../ffmpeg-custom-config.sh"
+
 FF_CFLAGS=""
 FF_CXXFLAGS=""
 FF_LDFLAGS=""
-GIT_BRANCH="master"
+# 使用集中配置中指定的稳定版本
+GIT_BRANCH="release/${FFMPEG_VERSION}"
 LICENSE_FILE="COPYING.GPLv3"
